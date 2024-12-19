d3fc63be0dd5c230de4e9e7ae0a7b2763960b922
parasolya <ola.shevchuk@gmail.com>
2024-12-19 13:15:56 +0200

    update signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,14 @@ import bcrypt from "bcryptjs";
 
 import User from "../models/userModel.js";
 import ctrlWrapper from "../decorators/ctrlWrapper.js";
+import HttpError from "../helpers/HttpError.js";
 
 const signup = async (req, res) => {
-  const { password } = req.body;
+  const { email, password } = req.body;
+  const user = await User.findOne({email});
+  if (user) {
+    throw HttpError(409, "email in use")
+  }
   const hashPassword = await bcrypt.hash(password, 10);
   const newUser = await User.create({ ...req.body, password: hashPassword });
 
